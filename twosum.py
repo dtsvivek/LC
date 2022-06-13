59ade7b9b425f6eaf96f53efe14d1a2aa79a650b
dtsvivek <95055534+dtsvivek@users.noreply.github.com>
2022-06-12 22:42:57 -0400

    first commit

---
+++ twosum.py
@@ -0,0 +1,23 @@
+# %%
+
+from collections import OrderedDict
+class LRUCache:
+
+    cache = OrderedDict()
+    
+    cap = 0
+    
+    def __init__(self, capacity: int):
+        
+        self.cap = capacity
+        return
+
+    def get(self, key: int) -> int:
+        
+        return 0
+
+    def put(self, key: int, value: int) -> None:
+        
+        return
+
+# %%
