bc719b0cbb8c4dafbbbe432e2f877a3c37106c73
dtsvivek <vivek.teja.raju@gmail.com>
2022-08-11 01:37:13 +0530

    kth smallest element

---
+++ 230.kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=230 lang=cpp
+ *
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (68.05%)
+ * Likes:    7912
+ * Dislikes: 137
+ * Total Accepted:    888.8K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given the root of a binary search tree, and an integer k, return the k^th
+ * smallest value (1-indexed) of all the values of the nodes in the tree.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,1,4,null,2], k = 1
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * Output: 3
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is n.
+ * 1 <= k <= n <= 10^4
+ * 0 <= Node.val <= 10^4
+ * 
+ * 
+ * 
+ * Follow up: If the BST is modified often (i.e., we can do insert and delete
+ * operations) and you need to find the kth smallest frequently, how would you
+ * optimize?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void helper(TreeNode* root){
+        allnumbers.push_back(root->val);
+        if(root->left){
+            helper(root->left);
+        }if(root->right){
+            helper(root->right);
+        }
+    }
+
+public:
+    vector<int> allnumbers;
+    int kthSmallest(TreeNode* root, int k) {
+        helper(root);
+        sort(allnumbers.begin(),allnumbers.end());
+        return allnumbers[k-1];
+    }
+};
+// @lc code=end
+
