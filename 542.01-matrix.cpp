be06c5b70d3da70ac3748a112b881de535639e07
dtsvivek <vivek.teja.raju@gmail.com>
2023-01-07 19:39:10 +0530

    Back to grind

---
+++ 542.01-matrix.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=542 lang=cpp
+ *
+ * [542] 01 Matrix
+ *
+ * https://leetcode.com/problems/01-matrix/description/
+ *
+ * algorithms
+ * Medium (43.88%)
+ * Likes:    5848
+ * Dislikes: 291
+ * Total Accepted:    317.7K
+ * Total Submissions: 719.8K
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * Given an m x n binary matrix mat, return the distance of the nearest 0 for
+ * each cell.
+ * 
+ * The distance between two adjacent cells is 1.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+ * Output: [[0,0,0],[0,1,0],[0,0,0]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+ * Output: [[0,0,0],[0,1,0],[1,2,1]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 10^4
+ * 1 <= m * n <= 10^4
+ * mat[i][j] is either 0 or 1.
+ * There is at least one 0 in mat.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        
+    }
+};
+// @lc code=end
+
